f3d767580253bceb5d2912d69265153626f1744c
Nadi Nasser Mass3od <108473318+NadiNasserMass3od@users.noreply.github.com>
2024-10-08 22:22:37 +0300

    Delete lib directory

--- lib/Colors/color.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color blue = const Color(0xff1e90ec);
-Color red = const Color(0xFFF44336);
-Color white = const Color(0xffffffff);
-Color black = const Color(0xff000000);
